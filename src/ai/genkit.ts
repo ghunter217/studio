0ac40fb8d4850a24562747052153ce911aeacdb5
Diahmin hawkins <hawkinsdiahmin124@gmail.com>
2025-08-14 05:00:02 +0000

    No i want to place it in the /api/genkit] directly, if not, i can not ru

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,5 +2,5 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: 'AIzaSyBkMZCJddX9r6okt0Jm_6XD_B3zpHKVPzA'})],
 });
